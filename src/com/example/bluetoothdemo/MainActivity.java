b647729df7ebed060fc089a326a1988974bf6835
xiaoshan <xiaoshan.chu@ehking.com>
2015-12-03 15:21:51 +0800

    提交

--- src/com/example/bluetoothdemo/MainActivity.java
+++ src/com/example/bluetoothdemo/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.bluetoothdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
+	
+	private ListView lv;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		init();
+	}
+	
+	private void init(){
+		lv = (ListView) findViewById(R.id.lv_device_list);
+		lv.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				
+			}
+		});
 	}
 }
